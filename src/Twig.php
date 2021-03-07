fa0f218f113708ac29abc23072427f3d50988c16
Heinrich Schiller <heinrich@heinrich-schiller.de>
2021-03-07 19:42:20 +0100

    update

--- src/Twig.php
+++ src/Twig.php
@@ -7,7 +7,6 @@ namespace Slim\Views;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\ViewInterface;
 use Twig\Environment;
-use Twig\Loader\ArrayLoader;
 use Twig\Loader\FilesystemLoader;
 
 class Twig implements ViewInterface
@@ -16,18 +15,16 @@ class Twig implements ViewInterface
 
     public function __construct($loader, array $options = [])
     {
-        if( is_array($loader) ) {
-            $loader = new ArrayLoader($loader);
-        } else {
-            $loader = new FilesystemLoader($loader);
-        }
+        $loader = new FilesystemLoader($loader);
 
         $this->environment = new Environment($loader, $options);
     }
 
     public function render(Response $response, string $template, array $data = []): Response
     {
-        $this->environment->render($template, $data);
+        $html = $this->environment->render($template, $data);
+
+        $response->getBody->write($html);
 
         return $response;
     }
