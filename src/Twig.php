22a7bb08733a39a316029601841f2b5b230f3554
Heinrich Schiller <heinrich@heinrich-schiller.de>
2021-03-08 07:53:40 +0100

    added, phpdoc comments

--- src/Twig.php
+++ src/Twig.php
@@ -11,19 +11,32 @@ use Twig\Loader\FilesystemLoader;
 
 class Twig implements ViewInterface
 {
+    /**
+     * @var Environment
+     */
     private Environment $environment;
 
-    public function __construct($loader, array $options = [])
+    /**
+     * @param string $loader Path to templates
+     * @param array $options Twig options
+     */
+    public function __construct(string $loader, array $options = [])
     {
         $loader = new FilesystemLoader($loader);
 
         $this->environment = new Environment($loader, $options);
     }
 
+    /**
+     * Output rendered template
+     * 
+     * @param Response $response
+     * @param string $template Name of the template
+     * @param array $data Template variables
+     */
     public function render(Response $response, string $template, array $data = []): Response
     {
         $html = $this->environment->render($template, $data);
-
         $response->getBody()->write($html);
 
         return $response;
